ce267d86d975887ad1a4ebf1175bc5c3bd790256
mehmetsukrukavak <mehmetsukru.kavak07@gmail.com>
2019-06-04 23:06:44 +0300

    added Detail Page and added some new features

--- lib/products.dart
+++ lib/products.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_course/pages/product.dart';
 class Products extends StatelessWidget {
   final List<String> products;
 
@@ -11,6 +11,26 @@ class Products extends StatelessWidget {
         children: <Widget>[
           Image.asset('assets/food.jpg'),
           Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text(
+                  'Details',
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return ProductPage();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ],
       ),
     );
